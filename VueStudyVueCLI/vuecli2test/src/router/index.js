4bdc7704e475a1a077b20c648a6863327d6f2f71
Mrzhou-hr <211535176@qq.com>
2020-04-11 10:21:56 +0800

    cli

--- VueStudyVueCLI/vuecli2test/src/router/index.js
+++ VueStudyVueCLI/vuecli2test/src/router/index.js
@@ -5,9 +5,13 @@ import About from "../components/about";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
-    { path: "", component: HelloWorld },
-    { path: "/about", component: About }
+    { path: "", component: HelloWorld, meta: { title: "你好" } },
+    { path: "/about", component: About, meta: { title: "关于" } }
   ]
 });
+router.beforeEach((to, from, next) => {
+  document.title = to.matched[0].meta;
+});
+export default router;
